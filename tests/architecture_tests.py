be70a42ca29e48faf0eb7619f88b76829a348370
Pixelatory <nick.aksamit@hotmail.com>
2025-01-31 22:04:23 -0500

    Added vanilla transformer and a test

---
+++ tests/architecture_tests.py
@@ -0,0 +1,26 @@
+import unittest
+
+import torch
+
+from tests.common_tests import DIM_FEEDFORWARD
+from tests.multi_head_attention_tests import (
+    BATCH_SIZE,
+    D_MODEL,
+    NHEADS,
+    SEQ_LEN,
+)
+from transformers.architectures import VanillaTransformer
+
+VOCAB_SIZE = 8
+
+
+class ArchitectureTests(unittest.TestCase):
+    def test_vanilla_transformer(self):
+        input = torch.randint(low=0, high=VOCAB_SIZE, size=(BATCH_SIZE, SEQ_LEN))
+        transformer = VanillaTransformer(
+            VOCAB_SIZE, D_MODEL, NHEADS, DIM_FEEDFORWARD, 0.1, 4, 4
+        )
+        x, encoder_out, decoder_out = transformer(input)
+        self.assertEqual(encoder_out.shape, (BATCH_SIZE, SEQ_LEN, D_MODEL))
+        self.assertEqual(decoder_out.shape, (BATCH_SIZE, SEQ_LEN, D_MODEL))
+        self.assertEqual(x.shape, (BATCH_SIZE, SEQ_LEN, VOCAB_SIZE))
--- transformers/architectures.py
+++ transformers/architectures.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 
+from transformers.positional_encoding import SinusoidalEncoder
 from transformers.transformer import (
     TransformerDecoder,
     TransformerDecoderLayer,
@@ -12,6 +13,7 @@ from transformers.transformer import (
 class VanillaTransformer(nn.Module):
     def __init__(
         self,
+        vocab_size: int,
         d_model: int,
         n_heads: int,
         dim_feedforward: int,
@@ -24,6 +26,8 @@ class VanillaTransformer(nn.Module):
         bias: bool = True,
         fused_linear: bool = True,
         mha_args: dict | None = None,
+        padding_idx: int | None = None,
+        max_seq_len: int | None = None,
     ):
         super().__init__()
         encoder_layer = TransformerEncoderLayer(
@@ -53,6 +57,9 @@ class VanillaTransformer(nn.Module):
 
         self.encoder = TransformerEncoder(encoder_layer, num_encoder_layers)
         self.decoder = TransformerDecoder(decoder_layer, num_decoder_layers)
+        self.embedding = nn.Embedding(vocab_size, d_model, padding_idx=padding_idx)
+        self.pos_encoder = SinusoidalEncoder(max_seq_len=max_seq_len, d_model=d_model)
+        self.dense_out = nn.Linear(d_model, vocab_size)
 
     def forward(
         self,
@@ -61,6 +68,8 @@ class VanillaTransformer(nn.Module):
         self_attn_mask: torch.Tensor = None,
         cross_attn_mask: torch.Tensor = None,
     ):
+        x = self.embedding(x)
+        x = self.pos_encoder(x)
         encoder_out = self.encoder(x, mask=encoder_mask)
         decoder_out = self.decoder(
             x,
@@ -68,6 +77,5 @@ class VanillaTransformer(nn.Module):
             self_attn_mask=self_attn_mask,
             cross_attn_mask=cross_attn_mask,
         )
-
-        # TODO: positional encoding and embedding.
-        return encoder_out, decoder_out
+        x = self.dense_out(x)
+        return x, encoder_out, decoder_out
